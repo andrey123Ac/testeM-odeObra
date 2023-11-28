d5d7895d72375d84566d5cea65a9237029fceea3
andrey123Ac <139496165+andrey123Ac@users.noreply.github.com>
2023-11-28 12:15:05 -0300

    Update arquivo.js

--- arquivo.js
+++ arquivo.js
@@ -1,21 +1,28 @@
 function calcularCusto() {
-    const horasTrabalhadas = document.getElementById('horas').value;
-    const custoMateriais = document.getElementById('materiais').value;
-    const margemLucro = document.getElementById('margem').value;
-  
-    // Validação dos campos
-    if (horasTrabalhadas === "" || custoMateriais === "" || margemLucro === "") {
-      alert("Por favor, preencha todos os campos.");
-      return;
-    }
-  
-    const precoPorHora = 20; // Substitua pelo seu preço por hora
-    const custoMaoDeObra = horasTrabalhadas * precoPorHora;
-    const custoTotal = parseFloat(custoMaoDeObra) + parseFloat(custoMateriais);
-    const precoVenda = custoTotal * (1 + margemLucro / 100);
-  
-    document.getElementById('resultado').innerHTML = `Custo Total: $${custoTotal.toFixed(2)} | Preço de Venda: $${precoVenda.toFixed(2)}`;
+  const diasTrabalho = document.getElementById('dias').value;
+  const horasPorDia = document.getElementById('horas').value;
+  const custoMateriais = document.getElementById('materiais').value;
+  const quantidadeProdutos = document.getElementById('quantidade').value;
+  const margemLucro = document.getElementById('margem').value;
+
+  // Validação dos campos
+  if (diasTrabalho === "" || horasPorDia === "" || custoMateriais === "" || quantidadeProdutos === "" || margemLucro === "") {
+    alert("Por favor, preencha todos os campos.");
+    return;
   }
+
+  const precoPorHora = 20; // Substitua pelo seu preço por hora
+  const horasTotais = diasTrabalho * horasPorDia;
+  const custoMaoDeObra = horasTotais * precoPorHora;
+  const custoTotal = parseFloat(custoMaoDeObra) + parseFloat(custoMateriais);
+  const custoPorUnidade = custoTotal / quantidadeProdutos;
+  const precoVenda = custoTotal * (1 + margemLucro / 100);
+  const precoVendaPorUnidade = precoVenda / quantidadeProdutos;
+
+  document.getElementById('resultado').innerHTML = `Custo Total: $${custoTotal.toFixed(2)} | Custo por Unidade: $${custoPorUnidade.toFixed(2)} | Preço de Venda: $${precoVenda.toFixed(2)} | Preço de Venda por Unidade: $${precoVendaPorUnidade.toFixed(2)}`;
+}
+
+
 
   
   function calcularCusto2(){
